8acd5b82f22838f42014776d3602f9f6cde1c81d
Gabriel Acuña <gstef@hotmail.com>
2017-04-25 01:44:58 -0500

    Computer information

---
+++ app.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron');
+let window;
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+function createWindow() {
+  window = new BrowserWindow({
+    height: 768,
+    width: 1024,
+  });
+  window.darkTheme=true,
+  window.loadURL(`file://${__dirname}/index.html`);
+  window.on('closed', () => {
+  window = null
+ });
+}
